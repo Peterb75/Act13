0514d4904ad856761c2b1b408afaabe4eed16daa
Peterb75 <a20220250@utem.edu.mx>
2024-03-08 09:02:44 -0600

    Act13

---
+++ lib/movie.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: non_constant_identifier_names
+
+class MovieModel{
+  String movie_title;
+  int? movie_release_year;
+  String movie_poster_url; 
+  double? raiting;
+
+  MovieModel(this.movie_title, this.movie_release_year, this.raiting, this.movie_poster_url);
+
+}
